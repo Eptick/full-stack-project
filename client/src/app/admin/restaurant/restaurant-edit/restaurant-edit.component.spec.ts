909b82b80c5496312638a537a6606be94f4fe1cb
Leon Redžić <redzicleon@gmail.com>
2021-12-04 01:07:40 +0100

    admin restaurant base tests

--- client/src/app/admin/restaurant/restaurant-edit/restaurant-edit.component.spec.ts
+++ client/src/app/admin/restaurant/restaurant-edit/restaurant-edit.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BaloiseDesignSystemModule } from '@baloise/design-system-components-angular';
+import { RestaurantBackComponent } from '../restaurant-back/restaurant-back.component';
 
 import { RestaurantEditComponent } from './restaurant-edit.component';
 
@@ -8,7 +12,12 @@ describe('RestaurantEditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RestaurantEditComponent ]
+      imports: [
+        HttpClientModule,
+        RouterTestingModule,
+        BaloiseDesignSystemModule,
+      ],
+      declarations: [ RestaurantEditComponent, RestaurantBackComponent ]
     })
     .compileComponents();
   });
