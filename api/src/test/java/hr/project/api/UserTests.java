69b654b50eb331e5f9fa93478dcdc12b286294dc
Leon Redžić <redzicleon@gmail.com>
2021-12-04 23:14:26 +0100

    cleanup

--- api/src/test/java/hr/project/api/UserTests.java
+++ api/src/test/java/hr/project/api/UserTests.java
@@ -2,7 +2,6 @@ package hr.project.api;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.function.Consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -16,21 +15,17 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Commit;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.junit.Before;
 
 import hr.project.api.dto.UserDto;
 import hr.project.api.models.Role;
 import hr.project.api.models.User;
 import hr.project.api.repositories.RoleRepository;
-import hr.project.api.repositories.UserRepository;
 import hr.project.api.services.UserService;
 
 @RunWith(JUnit4.class)
@@ -46,8 +41,6 @@ public class UserTests {
     
     @Autowired
 	private RoleRepository roleRepository;
-    @Autowired
-	private UserRepository userRepository;
     @Autowired
 	private UserService userService;
     
