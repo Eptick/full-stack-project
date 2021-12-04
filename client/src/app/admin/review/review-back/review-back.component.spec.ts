587e468e81b2780584ae79fa0c8b06e916b6614e
Leon Redžić <redzicleon@gmail.com>
2021-12-04 01:27:13 +0100

    review base unit tests

--- client/src/app/admin/review/review-back/review-back.component.spec.ts
+++ client/src/app/admin/review/review-back/review-back.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BaloiseDesignSystemModule } from '@baloise/design-system-components-angular';
 
 import { ReviewBackComponent } from './review-back.component';
 
@@ -8,6 +10,10 @@ describe('ReviewBackComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        BaloiseDesignSystemModule,
+        RouterTestingModule,
+      ],
       declarations: [ ReviewBackComponent ]
     })
     .compileComponents();
--- client/src/app/admin/review/review-overview/review-overview.component.spec.ts
+++ client/src/app/admin/review/review-overview/review-overview.component.spec.ts
@@ -1,13 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BaloiseDesignSystemModule } from '@baloise/design-system-components-angular';
 
 import { ReviewOverviewComponent } from './review-overview.component';
 
-describe('ReviewOverviewComponent', () => {
+fdescribe('ReviewOverviewComponent', () => {
   let component: ReviewOverviewComponent;
   let fixture: ComponentFixture<ReviewOverviewComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        BaloiseDesignSystemModule,
+        RouterTestingModule,
+      ],
       declarations: [ ReviewOverviewComponent ]
     })
     .compileComponents();
