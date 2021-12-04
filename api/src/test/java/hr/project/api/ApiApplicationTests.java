173c9e12e51b806571943e17c0a5dde5d2965d4d
Leon Redžić <redzicleon@gmail.com>
2021-12-04 18:04:51 +0100

    test wip

--- api/src/test/java/hr/project/api/ApiApplicationTests.java
+++ api/src/test/java/hr/project/api/ApiApplicationTests.java
@@ -1,9 +1,11 @@
 package hr.project.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
+@AutoConfigureMockMvc
 class ApiApplicationTests {
 
 	@Test
