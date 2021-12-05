2082dde4bb43b5a6643744bab5b6056f2ef0cf9d
Leon Redžić <redzicleon@gmail.com>
2021-12-05 13:52:10 +0100

    RegisterComponent test

--- client/src/app/authentication/login/login.component.spec.ts
+++ client/src/app/authentication/login/login.component.spec.ts
@@ -12,10 +12,11 @@ import { AuthenticationService } from 'src/app/authentication.service';
 
 // Other imports
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { LoginComponent } from './login.component';
 import { RestaurantPageComponent } from 'src/app/restaurant/restaurant-page/restaurant-page.component';
+import { CONSTANTS } from 'src/app/constants';
 
 describe('LoginComponent', () => {
 
@@ -64,7 +65,7 @@ describe('LoginComponent', () => {
     component.loginForm.controls['username'].setValue('user');
     component.loginForm.controls['password'].setValue('user');
     loginElement.triggerEventHandler('ngSubmit', null);
-    const req = httpTestingController.expectOne(`http://localhost:8080/login`);
+    const req = httpTestingController.expectOne(`${CONSTANTS.API_URL}/login`);
     expect(req.request.method).toBe("POST");
     req.flush({ access_token: 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwicm9sZXMiOiJST0xFX0FETUlOIiwiZXhwIjoxNjM4NzkzMzIwfQ.bsRpGI8L3Egu81mdPsswY9w9nQw8-vs-2X2HjCXtyeIyqTuZ11cjMsAspbeFY3rYsRP_i4BYHdwsxHoUHHxp79bNqicbjISO2yTYp9QF-7sUrB-po2-Jvhwa3nEBVF1kvHFh3sraHkI0PImd58LVVKDrWNEc3jCl8au5YdKNN3ZSSp9jsu28RP-jDoiBvQf81KSqU42_S9c_1YaeI3eOk8XzsUbcjfKM3nMBHC71T55AhDEDYDQZEHh9lBq7Ez0okWlHpqgSyvmjkZ1CCbQoZtiCCODWbrmlPbfRnX64y75XM4zpdd9BJEBrEINF1nIkSJ--IUFO_sWAQ0fexCFVXg'}, {status: 200, statusText: ''});
 
--- client/src/app/authentication/register/register.component.spec.ts
+++ client/src/app/authentication/register/register.component.spec.ts
@@ -1,21 +1,45 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BaloiseDesignSystemModule } from '@baloise/design-system-components-angular';
 
 import { RegisterComponent } from './register.component';
 
-describe('RegisterComponent', () => {
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { LoginComponent } from '../login/login.component';
+import { DebugElement } from '@angular/core';
+import { AuthenticationService } from 'src/app/authentication.service';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { CONSTANTS } from 'src/app/constants';
+
+fdescribe('RegisterComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, RouterTestingModule, ReactiveFormsModule, BaloiseDesignSystemModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([
+          {path: 'login', component: LoginComponent},
+        ]),
+        ReactiveFormsModule,
+        BaloiseDesignSystemModule
+      ],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
+
+     // Inject the http service and test controller for each test
+     httpClient = TestBed.inject(HttpClient);
+     httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   beforeEach(() => {
@@ -27,4 +51,50 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call auth register method and transfer to login', waitForAsync(() => {
+    let registerElement: DebugElement;
+    const debugElement = fixture.debugElement;
+    let authService = debugElement.injector.get(AuthenticationService);
+    let router = debugElement.injector.get(Router);
+    let registerSpy = spyOn(authService , 'register').and.callThrough();
+    let navigateSpy = spyOn(router , 'navigate').and.callThrough();
+
+    registerElement = fixture.debugElement.query(By.css('form'));
+    // to set values
+    component.registerForm.controls['username'].setValue('user');
+    component.registerForm.controls['password'].setValue('user');
+    registerElement.triggerEventHandler('ngSubmit', null);
+    const req = httpTestingController.expectOne(`${CONSTANTS.API_URL}/register`);
+    expect(req.request.method).toBe("POST");
+    req.flush({ id: 1}, {status: 200, statusText: ''});
+
+    expect(registerSpy).toHaveBeenCalledTimes(1); // check that service is called once
+    expect(navigateSpy).toHaveBeenCalledTimes(1); // check that service is called once
+    expect(navigateSpy).toHaveBeenCalledWith(['/login'], { queryParams: { state: 'registered'}})
+
+   }));
+
+
+  it('should should show taken username', waitForAsync(() => {
+    let registerElement: DebugElement;
+    const debugElement = fixture.debugElement;
+    let authService = debugElement.injector.get(AuthenticationService);
+    let registerSpy = spyOn(authService , 'register').and.callThrough();
+
+
+    registerElement = fixture.debugElement.query(By.css('form'));
+    // to set values
+    component.registerForm.controls['username'].setValue('user');
+    component.registerForm.controls['password'].setValue('user');
+    registerElement.triggerEventHandler('ngSubmit', null);
+    const req = httpTestingController.expectOne(`${CONSTANTS.API_URL}/register`);
+    expect(req.request.method).toBe("POST");
+    req.flush(null, {status: 409, statusText: ''});
+
+    expect(registerSpy).toHaveBeenCalledTimes(1); // check that service is called once
+    expect(component.form.control.get('username')?.getError('taken')).toBeTruthy();
+
+   }));
+
 });
