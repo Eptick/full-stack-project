57af8598fa66923c4c4474b2ee5b430b2c28c65b
Leon Redžić <redzicleon@gmail.com>
2021-12-05 16:03:01 +0100

    remove unused module

--- client/src/app/restaurant/review/leave-a-review/leave-a-review.component.spec.ts
+++ client/src/app/restaurant/review/leave-a-review/leave-a-review.component.spec.ts
@@ -1,4 +1,3 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
